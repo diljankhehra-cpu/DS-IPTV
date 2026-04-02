2a14ed1649b021e0d2f6c19a060d35bcbf369ca9
diljankhehra-cpu <diljankhehra@gmail.com>
2026-04-02 18:13:53 +0530

    flashlight permission added

--- app/src/main/java/com/dsiptv/app/MainActivity.java
+++ app/src/main/java/com/dsiptv/app/MainActivity.java
@@ -1,16 +1,39 @@
 package com.dsiptv.app;
 
-import android.app.Activity;
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isOn = false;
+    private CameraManager cameraManager;
+    private String cameraId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView tv = new TextView(this);
-        tv.setText("DS IPTV 🚀 Working!");
-        setContentView(tv);
+        Button btn = findViewById(R.id.btnFlash);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (Exception e) {}
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    isOn = !isOn;
+                    cameraManager.setTorchMode(cameraId, isOn);
+                } catch (Exception e) {}
+            }
+        });
     }
 }
