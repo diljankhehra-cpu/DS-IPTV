183265d5aae8dbc1810b3fd3e0cdaeb3fa55a5f9
diljankhehra-cpu <diljankhehra@gmail.com>
2026-04-02 17:21:14 +0530

    first commit

---
+++ app/src/main/java/com/dsiptv/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dsiptv.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("DS IPTV 🚀 Working!");
+        setContentView(tv);
+    }
+}
